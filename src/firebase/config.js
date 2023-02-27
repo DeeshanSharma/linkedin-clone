e892643af5a84f5c1ee74a4bd18014a4b1c7d1d0
Conor <conorgould@hotmail.com>
2023-02-27 14:21:43 +0545

    Create infig.js
    sent from my iPhone

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCuQKv7jHTBhlbaz67_JC01QTlIcP3bKVc",
+  authDomain: "linkedout-358a2.firebaseapp.com",
+  projectId: "linkedout-358a2",
+  storageBucket: "linkedout-358a2.appspot.com",
+  messagingSenderId: "626431839505",
+  appId: "1:626431839505:web:32021e25db5c2527b290b0",
+  measurementId: "G-112CGB8CS0"
+};
