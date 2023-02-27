10f7494142cd6de5354e33402f795deebb5e3186
Conor <conorgould@hotmail.com>
2023-02-27 14:42:34 +0545

    Update index.js

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -2,8 +2,17 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 import "firebase/storage";
-import firebaseConfig from "./config";
 
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCuQKv7jHTBhlbaz67_JC01QTlIcP3bKVc",
+    authDomain: "linkedout-358a2.firebaseapp.com",
+    projectId: "linkedout-358a2",
+    storageBucket: "linkedout-358a2.appspot.com",
+    messagingSenderId: "626431839505",
+    appId: "1:626431839505:web:32021e25db5c2527b290b0",
+    measurementId: "G-112CGB8CS0"
+  };
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 const auth = firebase.auth();
